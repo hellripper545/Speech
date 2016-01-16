28238875c6ce0f3127583406ac743f935a31fd43
hellripper545 <hellripper545@gmail.com>
2016-01-16 23:11:26 +0530

    Adding a bit interactive using random function.

--- BunnyBot/Form1.cs
+++ BunnyBot/Form1.cs
@@ -27,7 +27,7 @@ namespace BunnyBot
         private void form1_Load(object sender, EventArgs e)
         {
             Choices commands = new Choices();
-            commands.Add(new string[] { "bunny", "hello", "hai", "hey", "what time is it", "what day is it" });
+            commands.Add(new string[] { "bunny", "hello", "hai", "hey", "what time is it", "what day is it", "goodbye", "bye", "see you" });
             GrammarBuilder gBuiler = new GrammarBuilder();
             gBuiler.Append(commands);
             Grammar grammer = new Grammar(gBuiler);
@@ -40,22 +40,45 @@ namespace BunnyBot
         private void User_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             string input = e.Result.Text;
+            int ranNum = new Random(1,10);
             switch (input)
             {
                 case "hai":
                 case "hello":
                 case "hey":
                 case "bunny":
-                    bunny.Speak("Hello");
+                    if (ranNum < 5)
+                    {
+                        bunny.Speak("Hello");
+                    }
+                    else
+                    {
+                        bunny.Speak("Hai");
+                    }
                     break;
+
                 case "what time is it":
                     DateTime now = new DateTime();
                     string time = now.GetDateTimeFormats('t')[0];
                     bunny.Speak(time);
                     break;
+
                 case "what date is it":
                     bunny.Speak(DateTime.Today.ToString("dd-MM-yyyy"));
                     break;
+
+                case "goodbye":
+                case "bye":
+                    if (ranNum < 5)
+                    {
+                        bunny.Speak("Farewell");
+                    }
+                    else
+                    {
+                        bunny.Speak("GoodBye");
+                    }
+                    break;
+                   
             }
         }
     }
