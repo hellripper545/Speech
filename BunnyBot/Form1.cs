a70f7c428af5df2f73c763184229637bdbca438a
hellripper545 <hellripper545@gmail.com>
2016-01-16 22:45:03 +0530

    Add speech recognition engine and small workaround for basic commands

--- BunnyBot/Form1.cs
+++ BunnyBot/Form1.cs
@@ -7,19 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Globalization;
 
 namespace BunnyBot
 {
     public partial class form1 : Form
     {
+        SpeechRecognitionEngine user = new SpeechRecognitionEngine();
+        SpeechSynthesizer bunny = new SpeechSynthesizer();
+
         public form1()
         {
             InitializeComponent();
         }
 
+        
         private void form1_Load(object sender, EventArgs e)
         {
-
+            Choices commands = new Choices();
+            commands.Add(new string[] { "bunny", "hello", "hai", "hey", "what time is it", "what day is it" });
+            GrammarBuilder gBuiler = new GrammarBuilder();
+            gBuiler.Append(commands);
+            Grammar grammer = new Grammar(gBuiler);
+            user.LoadGrammarAsync(grammer);
+            user.SetInputToDefaultAudioDevice();
         }
     }
 }
