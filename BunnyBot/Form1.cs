a14d46557f3b50639588ab8a567c379dbdf13da4
hellripper545 <hellripper545@gmail.com>
2016-01-16 23:55:08 +0530

    Add more interactiveness

--- BunnyBot/Form1.cs
+++ BunnyBot/Form1.cs
@@ -17,6 +17,8 @@ namespace BunnyBot
     {
         SpeechRecognitionEngine user = new SpeechRecognitionEngine();
         SpeechSynthesizer bunny = new SpeechSynthesizer();
+        string userName = Environment.UserName;
+        private int ranNum;
 
         public form1()
         {
@@ -27,40 +29,71 @@ namespace BunnyBot
         private void form1_Load(object sender, EventArgs e)
         {
             Choices commands = new Choices();
-            commands.Add(new string[] { "bunny", "hello", "hai", "hey", "what time is it", "what day is it", "goodbye", "bye", "see you" });
+            commands.Add(new string[] { "bunny", "hello", "hai", "hey", "hello bunny", "what is my name", "what time is it", "what day is it", "goodbye", "goodbye bunny", "close bunny", "go offline", "bye", "see you" });
             GrammarBuilder gBuiler = new GrammarBuilder();
             gBuiler.Append(commands);
             Grammar grammer = new Grammar(gBuiler);
             user.LoadGrammarAsync(grammer);
             user.SetInputToDefaultAudioDevice();
             user.SpeechRecognized += User_SpeechRecognized;
-                       
+                                   
         }
 
         private void User_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             string input = e.Result.Text;
-            int ranNum = new Random(1,10);
+            Random rnd = new Random();
+            DateTime now = new DateTime();
             switch (input)
             {
                 case "hai":
                 case "hello":
-                case "hey":
-                case "bunny":
-                    if (ranNum < 5)
+                case "hello bunny":
+                    if ( now.Hour >= 5 && now.Hour < 12)
                     {
-                        bunny.Speak("Hello");
+                        bunny.Speak("Good Morning" +userName );
                     }
-                    else
+                    if (now.Hour >= 12 && now.Hour < 18)
                     {
-                        bunny.Speak("Hai");
+                        bunny.Speak("Good Afternoon " +userName);
+                    }
+                    if (now.Hour >= 18 && now.Hour < 24)
+                    {
+                        bunny.Speak("Good Evening " +userName);
+                    }
+                    if (now.Hour < 5)
+                    {
+                        bunny.Speak("Hello " +userName + ", it's getting late");
+                    }
+                    break;
+                case "bunny":
+                case "hey":
+                    ranNum = rnd.Next(1, 5);
+                    if (ranNum == 1)
+                    {
+                        bunny.Speak("Yes sir");
+                    }
+                    else if (ranNum == 2)
+                    {
+                        bunny.Speak("Yes?");
+                    }
+                    else if (ranNum == 3)
+                    {
+                        bunny.Speak("How may I help?");
+                    }
+                    else if (ranNum == 4)
+                    {
+                        bunny.Speak("How may I be of assistance?");
                     }
                     break;
 
+                case "what is my name":
+                    bunny.Speak( " " +userName );
+                    break;
+
                 case "what time is it":
-                    DateTime now = new DateTime();
                     string time = now.GetDateTimeFormats('t')[0];
-                    bunny.Speak(time);
+                    bunny.Speak("time");
                     break;
 
                 case "what date is it":
@@ -69,13 +102,19 @@ namespace BunnyBot
 
                 case "goodbye":
                 case "bye":
-                    if (ranNum < 5)
+                case "goodbye bunny":
+                case "close bunny":
+                case "go offline":
+                case "see you":
+                    if (ranNum > 6)
                     {
                         bunny.Speak("Farewell");
+                        Close();
                     }
                     else
                     {
                         bunny.Speak("GoodBye");
+                        Close();
                     }
                     break;
                    
